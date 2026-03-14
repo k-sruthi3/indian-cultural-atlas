54844359274d81080045f626ce62aadde763687a
sruthi <ksruthi135@gmail.com>
2026-03-14 16:02:41 +0530

    Improve contact page UI and customize password reset templates

--- culture/forms.py
+++ culture/forms.py
@@ -18,8 +18,35 @@ class ProfileUpdateForm(forms.ModelForm):
 
 from django import forms
 
+
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    subject = forms.CharField(max_length=150)
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Enter your full name"
+    }))
+
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "class": "form-control",
+        "placeholder": "Enter your email"
+    }))
+
+    subject = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Subject"
+    }))
+
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 4,
+        "placeholder": "Write your message here..."
+    }))
+
+from django import forms
+
+class CustomPasswordResetForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "class": "form-control form-control-lg",
+            "placeholder": "Enter your registered email"
+        })
+    )
